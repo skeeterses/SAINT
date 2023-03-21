99bb6cb69f5cedf852c4b1b95f6d0e27d8289bdf
skeeterses <42122332+skeeterses@users.noreply.github.com>
2023-03-21 13:48:47 -0500

    Add files via upload

---
+++ Saint.rkt
@@ -0,0 +1,61 @@
+#lang racket
+; The 26 elementary forms (elf) from Jame's Slagle's 1961 Saint calculus integration
+; program.
+; The syntax of a saint integration function is
+; (integrate '(function))
+; Some of the math functions will have to be written out in order to do any numeric
+; computations.
+
+(define elf (lambda (function)
+              ; Test the trivial case of integral dv = v             
+              (if (symbol? function)
+                  (if (dv? (symbol->string function))
+                      (string-ref (symbol->string function) 1)
+                      #f)
+                  (if (dv? (symbol->string (last function)))
+                      (let ([variable (string-ref (symbol->string (last function)) 1)])
+                        ; integral Cdv = Cv
+                        (if (= 2 (length function))
+                            (cond
+                              [(number? (car function)) (list (car function) variable)]
+                              [(and (char? (car function))
+                                    (char-upper-case? (car function))) (list (car function) variable)])
+                            '()))
+                      #f))))
+                        
+                        
+
+; Check the format to see if a string is 2 letters and 
+; d folowed by lowercase letter.
+; dv is true but dV is false and daa is false
+(define dv? (lambda (dv)
+              (if (string? dv)
+                  (if (= (string-length dv) 2)
+                      (if (and (char=? (string-ref dv 0) #\d)
+                               (char-lower-case? (string-ref dv 1)))
+                          #t
+                          #f)
+                      #f)
+                  #f)))
+
+
+; The first part of uSubstitution is complete.
+; uSubstitution of u=sinx for ( sinx cosx dx) results in (u cosx dx)
+; The next step is to replace (cosx dx) with du.
+(define uSubstitution (lambda (u fx function)
+                        (if (null? function)
+                            '()
+                          (let ([head (car function)])
+                          (if (equal? fx head)
+                              (append (list u) (uSubstitution u fx (cdr function)))
+                              (if (list? head)
+                                  (append (list (uSubstitution u fx head))
+                                          (uSubstitution u fx (cdr function)))
+                                  (append (list head) (uSubstitution u fx (cdr function)))))))))
+
+                        
+                        
+
+
+              
+              
