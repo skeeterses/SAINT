62bac95ec5db1cefcb28bae615f11c1bb231399c
skeeterses <42122332+skeeterses@users.noreply.github.com>
2023-03-23 13:17:05 -0500

    Add files via upload

--- Saint.rkt
+++ Saint.rkt
@@ -8,20 +8,67 @@
 
 (define elf (lambda (function)
               ; Test the trivial case of integral dv = v             
-              (if (symbol? function)
-                  (if (dv? function)
-                      (string-ref function 1)
-                      #f)
-                  (if (dv? (last function))
-                      (let ([variable (string-ref (last function) 1)])
-                        ; integral Cdv = Cv
-                        (if (= 2 (length function))
-                            (cond
-                              [(number? (car function)) (list (car function) variable)]
-                              [(and (char? (car function))
-                                    (char-upper-case? (car function))) (list (car function) variable)])
-                            '()))
-                      #f))))
+              (let ([funLength (length function)] [dv (last function)])
+                (if (dv? dv)
+                    (let ([variable (string-ref dv 1)])
+                      (cond
+                        ; Trivial case of integral dv = v
+                        [(= funLength 1) (string-ref dv 1)]
+                        [(= funLength 2)
+                           (let ([mainFunction (car function)])
+                           (cond [(number? mainFunction) (list mainFunction variable)]
+                                 [(and (char? mainFunction)
+                                     (char-upper-case? mainFunction)) (list mainFunction variable)]   ; Rule a) Cdv -> Cv.
+                                 [(equal? mainFunction variable)
+                                  (list '/ (list 'expt variable '2) '2)]   ; Rule u-i) vdv -> 1/2 * v^2
+                                 [(list? mainFunction)
+                                  (cond
+                                    [(and (equal? 'exp (car mainFunction))
+                                          (equal? variable (cadr mainFunction)))
+                                     mainFunction]  ; Rule b) (e^x dx --> e^x
+                                    [(and (equal? 'expt (car mainFunction))
+                                          (equal? variable (caddr mainFunction))
+                                          (or (number? (cadr mainFunction))
+                                              (char? (cadr mainFunction))))
+                                     (list '/ (list 'expt (cadr mainFunction) variable) (list 'log (cadr mainFunction)))]
+                                      ; Rule c above: c^v dv = c^v / ln c
+                                    [(and (equal? 'log (car mainFunction)) (equal? variable (cadr mainFunction)))
+                                     (list '- (list variable (list 'log variable)) variable)]  ; Rule d: ln v --> vlnv - v
+                                    [(and (equal? 'log (car mainFunction))
+                                          (equal? variable (caddr mainFunction))
+                                          (or (number? (cadr mainFunction))
+                                              (char? (cadr mainFunction))))
+                                     (list '- (list variable 'log (cadr mainFunction) variable)
+                                              (list '/ variable (list 'log (cadr mainFunction))))] ; Rule e
+                                    [(and (equal? 'sin (car mainFunction)) (equal? variable (cadr mainFunction)))
+                                     (list '- 'cos variable)] ; Rule f
+                                    [(and (equal? 'cos (car mainFunction)) (equal? variable (cadr mainFunction)))
+                                     (list 'sin variable)]  ; Rule g
+                                    [(and (equal? 'tan (car mainFunction)) (equal? variable (cadr mainFunction)))
+                                     (list 'log (list 'sec variable))] ; Rule h
+                                    [(and (equal? 'cot (car mainFunction)) (equal? variable (cadr mainFunction)))
+                                     (list 'log (list 'sin variable))] ; Rule i
+                                    [(and (equal? 'sec (car mainFunction)) (equal? variable (cadr mainFunction)))
+                                     (list 'log (list '+ (list 'sec variable) (list 'tan variable)))] ; Rule j
+                                    [(and (equal? 'csc (car mainFunction)) (equal? variable (cadr mainFunction)))
+                                     (list 'log (list '- (list 'csc variable) (list 'cot variable)))] ; Rule k
+                                    [else #f])]
+                                 [else #f]))]
+                        [else #f]))
+                    #f)))) ; The function doesn't have "dx" at the end.
+                                 
+                                 
+                                                  
+;                  (if (dv? (last function))
+;                      (let ([variable (string-ref (last function) 1)])
+;                        ; integral Cdv = Cv
+;                        (if (= 2 (length function))
+;                            (cond
+;                              [(number? (car function)) (list (car function) variable)]
+;                              [(and (char? (car function))
+;                                    (char-upper-case? (car function))) (list (car function) variable)])
+;                            '()))
+;                      #f))))
                         
                         
 
