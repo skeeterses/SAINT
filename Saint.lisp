2129db207b0655805ddf30a9aafa3d552e555195
skeeterses <42122332+skeeterses@users.noreply.github.com>
2023-05-06 15:02:30 -0500

    Add files via upload

--- Saint.lisp
+++ Saint.lisp
@@ -3,8 +3,29 @@
 
 (defun elf (func)
   (match func
-	 ((list 'exp a) (list 'exp a))
-	 ((list 'sin a) (list '- 'cos a))
-	 ((list 'cos a) (list 'sin a))))
+	 ((guard (list a b)
+		 (and (constant? a) (dx? b)))
+	  (list a (char b 1)))
+	 ((guard (list (list 'exp a) b)
+		 (dx? b))
+		 (list 'exp a))))
+;  (match func	 	 
+;	 ((list 'exp a) (list 'exp a))
+;	 ((list 'sin a) (list '- 'cos a))
+;	 ((list 'cos a) (list 'sin a))
+	
 	  
+(defun dx? (elem)
+  (if (stringp elem)
+    (and (= (length elem) 2)
+	 (char= (char elem 0) #\d)
+	 (alpha-char-p (char elem 1)))
+    nil))
 
+(defun constant? (elem)
+  (cond ((numberp elem) elem)
+	((characterp elem)
+	 (if (upper-case-p elem)
+	   elem
+	   nil))
+	 (t nil)))
